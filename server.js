e80f86fce41ca243b978d01ef9b02b56f2fe342d
Vegard Flatoy <vegard@Vegards-MacBook-Pro.local>
2019-11-28 23:39:57 +0100

    add env port support

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const port = 3000
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
